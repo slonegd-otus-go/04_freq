3645f3d5c6cfb1bc2054bfb85174a67f54d742d9
slonegd <slonkdv@mail.ru>
2019-06-15 17:13:18 +0300

    change after review

--- freq.go
+++ freq.go
@@ -6,33 +6,19 @@ import (
 )
 
 func Calculate(in string, qty int) []string {
-	separators := []string{",", ".", "?", "!", "\t", "\n"}
-	for _, separator := range separators {
-		in = strings.Replace(in, separator, " ", -1)
-	}
-
-	words := strings.Split(in, " ")
+	words := strings.Fields(in)
 
 	wordsQty := map[string]int{}
 	for _, word := range words {
-		if word != "" {
-			wordsQty[word]++
-		}
+		wordsQty[word]++
 	}
 
-	elements := slice(wordsQty)
+	elements := sliceElements(wordsQty)
 	sort.Slice(elements, func(i, j int) bool {
 		return elements[i].qty > elements[j].qty
 	})
 
-	res := make([]string, 0, qty)
-	for i, element := range elements {
-		res = append(res, element.word)
-		if i == qty-1 {
-			break
-		}
-	}
-	return res
+	return sliceStrings(elements, qty)
 }
 
 type element struct {
@@ -40,10 +26,21 @@ type element struct {
 	qty  int
 }
 
-func slice(in map[string]int) []element {
+func sliceElements(in map[string]int) []element {
 	res := make([]element, 0, len(in))
 	for word, qty := range in {
 		res = append(res, element{word, qty})
+	}
+	return res
+}
+
+func sliceStrings(elements []element, maxLen int) []string {
+	res := make([]string, 0, maxLen)
+	for i, element := range elements {
+		res = append(res, element.word)
+		if i == maxLen-1 {
+			break
+		}
 	}
 	return res
 }
